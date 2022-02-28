6f39cc0538147d13a1f4a82fcb902a8b7509a3fd
Franco <francoramiresdosanto@gmail.com>
2022-02-28 19:46:47 -0300

    Exemplos de Destructuring

---
+++ destructuring.js
@@ -0,0 +1,24 @@
+const numerosPares = [2, 4, 6]
+const numerosImpares = [1, 3, 5]
+
+const numeros = [...numerosPares, ...numerosImpares]
+
+const [num1, num2, ...outrosNumeros] = [1, 2, 3, 4, 5]
+
+const pessoa = {
+    nome: "Ju",
+    idade: 25
+}
+
+const pessoaComTelefone = {
+    ...pessoa, 
+    telefone: "32164987654"
+}
+
+const {nome} = pessoa
+
+function imprimeDados({nome, idade}) {
+    console.log(nome, idade)
+}
+
+imprimeDados(pessoa)
