e146bea712c40b0d4ccc024b71e240d4b9130258
Franco <francoramiresdosanto@gmail.com>
2022-03-03 08:29:20 -0300

    Trabalhando com laços de repetição e algoritmos de ordenação.

---
+++ seleciontSort.js
@@ -0,0 +1,17 @@
+const livros = require('./lista-livros')
+const menorValor = require('./menorValor')
+
+for (let atual = 0; atual < livros.length; atual++) {
+    
+    let menor = menorValor(livros, atual)
+
+    let livroAtual = livros[atual]
+    console.log("Livro atual", livros[atual])
+
+    let livroMenorPreco = livros[menor]
+    console.log("Livro menor preço", livros[menor])
+    
+    livros[atual] = livroMenorPreco
+    livros[menor] = livroAtual
+
+} 
